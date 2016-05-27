c2c197a1822a3f997945c498d6ed4c56644a4c20
brunob <bruno@eliaz.fr>
2016-05-27 11:58:56 +0000

    autoriser l'accès au privé uniquement pour les webmestres

--- octopouce_options.php
+++ octopouce_options.php
@@ -3,3 +3,8 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 define('_SELECTEUR_GENERIQUE_ACTIVER_PUBLIC', true);
+
+// autoriser le prive uniquement pour les webmestres
+function autoriser_ecrire($faire, $type, $id, $qui, $opt) {
+	return autoriser('webmestre', null, null, $qui, $opt);
+}
