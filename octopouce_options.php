82d537547d75153e97ca86104489701ec9112000
brunob <bruno@eliaz.fr>
2016-05-27 15:09:14 +0000

    ne pas se faire polluer la css perso par css_imbriquees (va falloir qu'on trouve un moyen de ne pas subir ce filtre qui fait gonfler les css)

--- octopouce_options.php
+++ octopouce_options.php
@@ -4,6 +4,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 define('_SELECTEUR_GENERIQUE_ACTIVER_PUBLIC', true);
 
+define('_UTILISER_PIE_HTC', false);
+define('_UTILISER_BOXSIZING_HTC', false);
+
 // autoriser le prive uniquement pour les webmestres
 function autoriser_ecrire($faire, $type, $id, $qui, $opt) {
 	return autoriser('webmestre', null, null, $qui, $opt);
