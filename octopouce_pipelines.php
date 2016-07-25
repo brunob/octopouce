47c20d7f0f2a2c02dc46dfbafd1f510592719815
brunob <bruno@eliaz.fr>
2016-07-25 12:57:51 +0000

    page people : afficher la bio des auteurs dans une tooltip avec tooptipster v3.3 (la 4.x est bien trop lourde)

--- octopouce_pipelines.php
+++ octopouce_pipelines.php
@@ -3,6 +3,19 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
+ * Insertion dans le pipeline insert_head (SPIP)
+ * Ajouter les scripts nécessaires au projet au head des pages
+ *
+ * @pipeline pre_edition
+ * @param array $flux Données du pipeline
+ * @return array      Données du pipeline
+ */
+function octopouce_insert_head($flux){
+	$flux .="\n".'<script type="text/javascript" src="'. find_in_path('javascript/jquery.tooltipster.js') .'"></script>';
+	return $flux;
+}
+
+/**
  * Insertion dans le pipeline pre_edition (SPIP)
  * Ajouter les valeurs des extras lors de la soumission des forms inscription & profil
  *
