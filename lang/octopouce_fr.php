1a6621f47fc9e81a582721e315008a57656f1a3c
brunob <bruno@eliaz.fr>
2017-06-19 12:20:13 +0000

    item de langue

--- lang/octopouce_fr.php
+++ lang/octopouce_fr.php
@@ -48,7 +48,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	'titre_editer_poulpe' => 'Modifier un compte utilisateur',
 	'titre_editer_poulpe_nouveau' => 'Nouveau compte utilisateur',
-	'titre_home_live' => '<em>#live pouits</em> du réseau',
+	'titre_home_live' => '<em>#infos</em> du réseau',
 	'titre_home_rejoindre' => 'Rejoingez les <em>@nb@ poulpes</em> du réseau',
 	'titre_octo_hasard' => 'Octo hasard (8 au hasard)',
 	'titre_page_people' => 'Tous les poulpes',
