7068f5e959e5b11b7af53581d404ca277e76132b
brunob <bruno@eliaz.fr>
2017-10-30 18:28:24 +0000

    brancher la page d'acceuil sur le compte @octopouce (par un define)

--- octopouce_options.php
+++ octopouce_options.php
@@ -7,6 +7,8 @@ define('_SELECTEUR_GENERIQUE_ACTIVER_PUBLIC', true);
 define('_UTILISER_PIE_HTC', false);
 define('_UTILISER_BOXSIZING_HTC', false);
 
+define('_OCTOPOUCE_AUTEUR', 3);
+
 // autoriser le prive uniquement pour les webmestres
 function autoriser_ecrire($faire, $type, $id, $qui, $opt) {
 	return autoriser('webmestre', null, null, $qui, $opt);
