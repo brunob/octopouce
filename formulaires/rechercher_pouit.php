17a7f0147c709fe2f2e0dd68fa2c96181a9ab51c
brunob <bruno@eliaz.fr>
2018-07-25 13:40:35 +0000

    ménage

--- formulaires/rechercher_pouit.php
+++ formulaires/rechercher_pouit.php
@@ -14,23 +14,3 @@ function formulaires_rechercher_pouit_charger_dist() {
 			'quand' => _request('quand')
 		);
 }
-/*
-function formulaires_rechercher_pouit_traiter_dist() {
-	$res = array('editable' => true);
-
-	// construire la chaine de tags pour la recherche
-	$recherche = _request('type_message') . ' ' . _request('depart') . '_' . _request('arrivee');
-	if (_request('date', _request('date'))) {
-		$recherche .= ' #' . _request('date', _request('date'));
-	}
-	if (_request('heure', _request('date'))) {
-		$recherche .= ' ' . str_replace(':', 'h', _request('heure', _request('date')));
-	}
-
-	$cible = parametre_url('recherche', 'recherche', $recherche, '&');
-
-	include_spip('inc/headers');
-	$res['redirect'] = $cible;
-	return $res;
-}
-*/
