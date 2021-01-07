152d2697ce1a1ec7fd4867e875e153c4b6697670
brunob <bruno@eliaz.fr>
2021-01-07 11:51:18 +0100

    ménage

--- lang/octopouce_fr.php
+++ lang/octopouce_fr.php
@@ -65,7 +65,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_editer_poulpe_nouveau' => 'Nouveau compte utilisateur',
 	'titre_home_fb' => 'Retrouvez-nous sur facebook',
 	'titre_home_live' => 'Infos du réseau',
-	'titre_home_raisons' => '2 bonnes raisons de faire du stop',
 	'titre_menu_qui' => 'Qui sommes nous ?',
 	'titre_octo_hasard' => 'Octo hasard (8 au hasard)',
 	'titre_page_people' => 'Tous les poulpes',
